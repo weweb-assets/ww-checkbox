f53bc90a695ec09d5a13590bbeb478579e786a24
Marc Fabre <nds.kram@gmail.com>
2022-03-23 14:49:05 +0100

    first commit

---
+++ src/wwElement.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="ww-icon" :style="style">
+        <div :class="[content.icon]" aria-hidden="true"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        content: { type: Object, required: true },
+    },
+    computed: {
+        style() {
+            return {
+                color: this.content.color,
+                fontSize: `${this.content.fontSize}px`,
+            };
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.ww-icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
