f21b1448e82c91e9ec3f60dc25f69c991ecfe4cd
Léo Coletta <mail.leo.coletta@gmail.com>
2025-06-19 19:59:18 +0200

    fix: Always show icon like main version, remove unnecessary state logic

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -24,58 +24,19 @@ export default {
             }
         }, { immediate: true });
 
-        const internalStates = ref([]);
-
         return {
             iconText,
-            internalStates,
         };
     },
-    mounted() {
-        console.log('ww-checkbox mounted:', { content: this.content, states: this.states });
-        this.internalStates = [...this.states];
-        
-        // Listen for state updates from parent
-        this.$el.addEventListener('updateCheckboxStates', (event) => {
-            console.log('ww-checkbox received updateCheckboxStates event:', event.detail);
-            this.internalStates = [...event.detail.states];
-        });
-    },
-    watch: {
-        states: {
-            handler(newStates, oldStates) {
-                console.log('ww-checkbox states watcher:', { newStates, oldStates });
-                this.internalStates = [...newStates];
-            },
-            immediate: true,
-            deep: true
-        }
-    },
     computed: {
-        isChecked() {
-            const checked = this.internalStates.includes('checked');
-            console.log('ww-checkbox isChecked:', { internalStates: this.internalStates, checked });
-            return checked;
-        },
         iconHTML() {
-            console.log('ww-checkbox iconHTML computed:', { isChecked: this.isChecked, iconText: this.iconText });
-            
-            // Only show icon when checked
-            if (!this.isChecked) {
-                console.log('ww-checkbox: not checked, returning empty string');
-                return '';
-            }
-            
-            console.log('ww-checkbox: is checked, showing icon');
-            
+            // Always show icon, like the main version
             // Use a placeholder icon in editor mode when no icon is set
             /* wwEditor:start */
             if (!this.iconText) {
-                console.log('ww-checkbox: using fallback icon');
                 return '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"></path></svg>';
             }
             /* wwEditor:end */
-            console.log('ww-checkbox: using loaded icon');
             return this.iconText;
         },
         style() {
